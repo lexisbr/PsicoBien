fffe49de3439e1161fca75fc2fa0ef96a391a72b
Alejandro Barrios <josebarriosrodas@gmail.com>
2023-11-04 20:17:18 -0600

    Merge branches

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const logger = require("morgan");
-const cors = require("cors");
-
 
 const http = require("http");
 const cors = require("cors"); // Import cors module
